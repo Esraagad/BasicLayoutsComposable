06b76846eabeb46514946610c30ea1002b043c35
esraagad <israa.gad13@gmail.com>
2025-04-22 12:00:30 +0200

    -Implement favorite card.

--- app/src/main/java/com/example/basiclayoutscomposable/MainActivity.kt
+++ app/src/main/java/com/example/basiclayoutscomposable/MainActivity.kt
@@ -4,20 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,7 +43,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicLayoutsComposableTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SearchBar(modifier = Modifier.fillMaxWidth())
+//                    SearchBar(modifier = Modifier.fillMaxWidth())
+//                    AlignYourBodyElement(modifier = Modifier.padding(8.dp), drawable = R.drawable.ab1_inversions, text = R.string.ab1_inversions)
+                    FavoriteCollectionCard(
+                        modifier = Modifier.padding(innerPadding),
+                        drawable = R.drawable.fc1_short_mantras,
+                        text = R.string.fc1_short_mantras
+                    )
+
                 }
             }
         }
@@ -57,7 +78,50 @@ fun SearchBar(modifier: Modifier) {
     )
 }
 
-@Preview
+@Composable
+fun AlignYourBodyElement(modifier: Modifier, @DrawableRes drawable: Int, @StringRes text: Int) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(drawable),
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+            modifier = modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(text),
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun FavoriteCollectionCard(modifier: Modifier, @DrawableRes drawable: Int, @StringRes text: Int) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(255.dp)
+        ) {
+            Image(
+                painter = painterResource(drawable),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = modifier.size(80.dp)
+            )
+            Text(text = stringResource(text), style = MaterialTheme.typography.titleMedium)
+        }
+    }
+}
+
+//@Preview
 @Composable
 fun SearchBarPreview(showBackground: Boolean = true, backgroundColor: Long = 0xFFF5F0EE) {
     BasicLayoutsComposableTheme {
@@ -66,4 +130,31 @@ fun SearchBarPreview(showBackground: Boolean = true, backgroundColor: Long = 0xF
         }
     }
 
+}
+
+
+//@Preview
+@Composable
+fun AlignYourBodyElementPreview(
+    showBackground: Boolean = true, backgroundColor: Long = 0xFFF5F0EE
+) {
+    BasicLayoutsComposableTheme {
+        AlignYourBodyElement(
+            modifier = Modifier.padding(8.dp),
+            drawable = R.drawable.ab1_inversions,
+            text = R.string.ab1_inversions
+        )
+    }
+}
+
+@Preview
+@Composable
+fun FavoriteCollectionCardPreview(showBackground: Boolean = true, backgroundColor: Long = 0xFFF5F0EE) {
+    BasicLayoutsComposableTheme {
+        FavoriteCollectionCard(
+            modifier = Modifier.padding(8.dp),
+            drawable = R.drawable.fc1_short_mantras,
+            text = R.string.fc1_short_mantras
+        )
+    }
 }
