cbb850e246fc763f5c67d6fae75d77875e201a3a
esraagad <israa.gad13@gmail.com>
2025-04-23 10:14:05 +0200

    Implement YourBodyElement LazyRow.

--- app/src/main/java/com/example/basiclayoutscomposable/MainActivity.kt
+++ app/src/main/java/com/example/basiclayoutscomposable/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,6 +18,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -45,12 +49,12 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 //                    SearchBar(modifier = Modifier.fillMaxWidth())
 //                    AlignYourBodyElement(modifier = Modifier.padding(8.dp), drawable = R.drawable.ab1_inversions, text = R.string.ab1_inversions)
-                    FavoriteCollectionCard(
-                        modifier = Modifier.padding(innerPadding),
-                        drawable = R.drawable.fc1_short_mantras,
-                        text = R.string.fc1_short_mantras
-                    )
-
+//                    FavoriteCollectionCard(
+//                        modifier = Modifier.padding(innerPadding),
+//                        drawable = R.drawable.fc1_short_mantras,
+//                        text = R.string.fc1_short_mantras
+//                    )
+                    AlignYourBodyRow(Modifier)
                 }
             }
         }
@@ -79,7 +83,11 @@ fun SearchBar(modifier: Modifier) {
 }
 
 @Composable
-fun AlignYourBodyElement(modifier: Modifier, @DrawableRes drawable: Int, @StringRes text: Int) {
+fun AlignYourBodyElement(
+    modifier: Modifier = Modifier,
+    @DrawableRes drawable: Int,
+    @StringRes text: Int
+) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -97,6 +105,17 @@ fun AlignYourBodyElement(modifier: Modifier, @DrawableRes drawable: Int, @String
             style = MaterialTheme.typography.bodyMedium,
             modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
         )
+    }
+}
+
+@Composable
+fun AlignYourBodyRow(modifier: Modifier = Modifier) {
+    LazyRow(modifier = modifier,
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(alignYourBodyData) { item ->
+            AlignYourBodyElement(drawable = item.drawable, text = item.text)
+        }
     }
 }
 
@@ -132,7 +151,6 @@ fun SearchBarPreview(showBackground: Boolean = true, backgroundColor: Long = 0xF
 
 }
 
-
 //@Preview
 @Composable
 fun AlignYourBodyElementPreview(
@@ -149,7 +167,22 @@ fun AlignYourBodyElementPreview(
 
 @Preview
 @Composable
-fun FavoriteCollectionCardPreview(showBackground: Boolean = true, backgroundColor: Long = 0xFFF5F0EE) {
+fun AlignYourBodyRowPreview(
+    showBackground: Boolean = true, backgroundColor: Long = 0xFFF5F0EE
+) {
+    BasicLayoutsComposableTheme {
+        AlignYourBodyRow(
+            modifier = Modifier
+        )
+    }
+}
+
+//@Preview
+@Composable
+fun FavoriteCollectionCardPreview(
+    showBackground: Boolean = true,
+    backgroundColor: Long = 0xFFF5F0EE
+) {
     BasicLayoutsComposableTheme {
         FavoriteCollectionCard(
             modifier = Modifier.padding(8.dp),
@@ -158,3 +191,26 @@ fun FavoriteCollectionCardPreview(showBackground: Boolean = true, backgroundColo
         )
     }
 }
+
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
